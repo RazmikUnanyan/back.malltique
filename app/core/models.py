0540975bfd5ff794512d8d8a603c0dc43d12b6ce
Razmik <outsource2@omnicampus.io>
2024-12-17 15:02:33 -0600

    fix migration

--- app/core/models.py
+++ app/core/models.py
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError("User mast have an email address.")
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -29,14 +29,15 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
+
         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """User model"""
+    """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
