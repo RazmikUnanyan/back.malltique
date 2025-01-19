0143f4d531d076053c4eed7e34209d796ce351bd
Razmik <razmik.unanyan@mail.ru>
2025-01-19 08:36:05 -0600

    fixed run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-putyon manage.py wait_for_db
-putyon manage.py collectstatic --noinput
-putyon manage.py migrate
+python manage.py wait_for_db
+python manage.py collectstatic --noinput
+python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
