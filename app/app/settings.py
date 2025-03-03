14d26de78f2a53d969b366d8c88760537ce43bc1
Razmik <razmik.unanyan@mail.ru>
2025-03-03 12:45:52 -0600

    update proxy

--- app/app/settings.py
+++ app/app/settings.py
@@ -154,8 +154,8 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
 }
 
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://runnerviewwithkat.com",
-]
+# CORS_ALLOW_ALL_ORIGINS = False
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "https://runnerviewwithkat.com",
+# ]
